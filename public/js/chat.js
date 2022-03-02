f5ca6308a2179d6b4fe91c521514c2c6d35341e3
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2022-03-02 22:12:27 +0800

    Added form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,48 @@
 const socket = io();
-const form = document.querySelector('#message-form');
+const messageForm = document.querySelector('#message-form');
+const messageFormButton = document.querySelector('#submit');
 const locationButton = document.querySelector('#send-location');
+
 
 socket.on('message', (message) => {
     console.log(message);
 });
 
-form.addEventListener('submit', (e) => {
+
+messageForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    message = e.target.elements.message;
+
+    messageFormButton.setAttribute('disabled', '');
+    message = this.elements.message;
+
     socket.emit('sendMessage', message.value, (error) => {
+        messageFormButton.removeAttribute('disabled');
+        message.value = '';
+        message.focus();
+
         if (error) {
             return console.log(error);
         }
         console.log('Message delivered!');
     });
-    message.value = '';
 });
 
-locationButton.addEventListener('click', () => {
+
+locationButton.addEventListener('click', function() {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
 
+    this.setAttribute('disabled', '');
+
     navigator.geolocation.getCurrentPosition((pos) => {
         const { latitude, longitude } = pos.coords;
         socket.emit('sendLocation', { latitude, longitude }, () => {
+            this.removeAttribute('disabled');
             console.log('Location shared!');
         });
     }, (err) => {
+        this.removeAttribute('disabled');
         console.warn(`ERROR(${err.code}): ${err.message}`);
     }, { enableHighAccuracy: true });
 });
